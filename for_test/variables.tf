6eeb33455f03f909d417985a93a910831c88b3c7
AKPySec <48283299+akpysec@users.noreply.github.com>
2021-06-12 12:41:17 +0300

    Update variables.tf

--- for_test/variables.tf
+++ for_test/variables.tf
@@ -1,1 +1,17 @@
-# for some tests
+variable "environment" {
+  type        = string
+  description = "Name you ENVIRONMENT"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "Your VPC CIDR"
+}
+
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.100.0/24",
+    "10.0.101.0/24",
+  ]
+}
